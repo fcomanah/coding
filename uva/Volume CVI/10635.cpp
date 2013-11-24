a0d9f5beb5da99c5d34a7853618946671bccef56
fcomanah <fco.manah@gmail.com>
2013-11-24 12:54:12 -0200

    Update 10635.cpp

--- uva/Volume CVI/10635.cpp
+++ uva/Volume CVI/10635.cpp
@@ -1,42 +1,110 @@
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <string>
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
-#include <queue>
-#include <set>
-#include <stack>
-#include <map>
-#include <list>
-#include <cmath>
-
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<cmath>
+ 
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<stack>
+#include<queue>
+#include<map>
+#include<sstream>
+ 
+ 
+ 
+#define FOR(i, s, e) for(int i=s; i<e; i++)
+#define loop(i, n) for(int i=0; i<n; i++)
+#define getint(n) scanf("%d", &n)
+#define pb(a) push_back(a)
+#define ll long long
+#define SZ(a) int(a.size())
+#define read() freopen("input.txt", "r", stdin)
+#define write() freopen("output.txt", "w", stdout)
+#define mem(a, v) memset(a, v, sizeof(a))
+#define all(v) v.begin(), v.end()
+#define pi acos(-1.0)
+#define INF 1<<29
+#define mod abs
+#define pf printf
+#define sf scanf
+ 
 using namespace std;
-
-typedef pair<int,int>   ii;
-typedef vector<int>     vi;
-typedef vector<ii>      vii;
-typedef vector<vi>     vvi;
-typedef vector<vii>    vvii;
-typedef map<int,int>    mii;
-
-
-int n,p,q;
-
-int main(){
-    int CC = 1,TC;
-    cin >> TC;
-    while(TC--){
-        cin >> n >> p >> q;
-        vi a(p+1),b(q+1);
-        for(int i=0;i<=p;i++){
-            cin >> a[i];
+ 
+#define MAXX 251
+ 
+int tmp, sc[MAXX*MAXX];
+int n, p, q;
+int pos[MAXX*MAXX];
+ 
+int LIS()
+{
+    int I[MAXX*MAXX + 1];
+    I[0] = -INF;
+ 
+    FOR(i, 1, q)
+    {
+        I[i] = INF;
+    }
+ 
+    int LisLength = 0;
+    loop(i, q)
+    {
+        int low, high, mid;
+        low = 0;
+        high = LisLength;
+ 
+        while(low <= high)
+        {
+            mid = (low+high)/2;
+            if(I[mid] < sc[i])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
         }
-        for(int i=0;i<=q;i++){
-            cin >> b[i];
+        I[low] = sc[i];
+        if(LisLength < low)
+        {
+            LisLength = low;
         }
     }
-
+    return LisLength;
+ 
+}
+ 
+int main()
+{
+    int kases, kaseno = 0;
+    getint(kases);
+    while(kases--)
+    {
+        getint(n); getint(p); getint(q);
+        p++;
+        q++;
+ 
+        mem(pos, -1);
+ 
+        loop(i, p)
+        {
+            getint(tmp);
+            pos[tmp] = i+1;
+        }
+ 
+        loop(i, q)
+        {
+            getint(tmp);
+            sc[i] = pos[tmp] - 1;
+ 
+        }
+ 
+        pf("Case %d: %d\n", ++kaseno, LIS() );
+ 
+    }
+ 
     return 0;
 }
